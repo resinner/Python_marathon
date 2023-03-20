812aefc6646dc8c388ab7b597bf33424ac18c316
resinner <revengingsinner@gmail.com>
2023-03-20 15:53:07 +0200

    marathon And

---
+++ main.py
@@ -0,0 +1,132 @@
+import pygame
+import random
+from os import listdir
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
+
+pygame.init()
+
+FPS =  pygame.time.Clock()
+
+screen = width, heigth = 800, 600
+
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+RED = 255, 0, 0
+GREEN = 0, 255, 0
+
+font = pygame.font.SysFont('Verdana', 20)
+
+main_surface = pygame.display.set_mode(screen)
+
+IMG_PATH = 'goose'
+
+player_imgs = [pygame.transform.scale(pygame.image.load(IMG_PATH + '/' + file ).convert_alpha(), (80,40)) for file in listdir(IMG_PATH  )]
+player = player_imgs[0]
+player_rect = player.get_rect()
+player_speed = 5
+
+def create_enemy():
+  enemy = pygame.transform.scale(pygame.image.load('enemy.png').convert_alpha(), (90,40))
+  enemy_rect = pygame.Rect(width, random.randint(0 + 40, heigth - 40), *enemy.get_size())
+  enemy_speed = random.randint(4, 6)
+  return [enemy, enemy_rect, enemy_speed]
+
+def create_bonus():
+  bonus = pygame.transform.scale(pygame.image.load('bonus.png').convert_alpha(), (140, 190))
+  bonus_rect = pygame.Rect( random.randint(0 + 140, width -140), 0 - 190, *bonus.get_size())
+  bonus_speed = random.randint(4, 6)
+  return [bonus, bonus_rect, bonus_speed]
+
+bg = pygame.transform.scale(pygame.image.load('background.png').convert(), screen)
+bgX = 0
+bgX2 = bg.get_width()
+bg_speed = 3
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 2500)
+
+CREATE_BONUS = pygame.USEREVENT + 2
+pygame.time.set_timer(CREATE_BONUS, 3500)
+
+CHANGE_IMG = pygame.USEREVENT + 3
+pygame.time.set_timer(CHANGE_IMG, 125)
+
+img_index = 0
+
+score = 0
+
+enemies = []
+bonuses = []
+
+is_working = True
+
+while is_working:
+   
+   FPS.tick(60)
+
+   for event in pygame.event.get():
+      if event.type == QUIT:
+         is_working = False
+
+      if event.type == CREATE_ENEMY:
+        enemies.append(create_enemy())
+
+      if event.type == CREATE_BONUS:
+        bonuses.append(create_bonus())
+
+      if event.type == CHANGE_IMG:
+        img_index += 1
+        if img_index == len(player_imgs):
+          img_index = 0
+        player = player_imgs[img_index]
+
+   pressed_keys = pygame.key.get_pressed()
+
+   bgX -= bg_speed
+   bgX2 -= bg_speed
+
+   if bgX < -bg.get_width():
+     bgX = bg.get_width()
+
+   if bgX2 < -bg.get_width():
+     bgX2 = bg.get_width()
+
+   main_surface.blit(bg, (bgX, 0))
+   main_surface.blit(bg, (bgX2, 0))
+   main_surface.blit(player, (player_rect))
+   main_surface.blit(font.render(str(score), True, BLACK), (width - 30, 0))
+
+   for enemy in enemies:
+     enemy[1] = enemy[1].move(-enemy[2], 0)
+     main_surface.blit(enemy[0], enemy[1])
+
+     if enemy[1].left < -100:
+       enemies.pop(enemies.index(enemy))
+
+     if player_rect.colliderect(enemy[1]):
+       is_working = False
+
+   for bonus in bonuses:
+     bonus[1] = bonus[1].move(0, bonus[2])
+     main_surface.blit(bonus[0], bonus[1])
+
+     if bonus[1].bottom >= heigth + 100:
+       bonuses.pop(bonuses.index(bonus))
+
+     if player_rect.colliderect(bonus[1]):
+       bonuses.pop(bonuses.index(bonus))
+       score += 1
+
+   if pressed_keys[K_DOWN] and not player_rect.bottom >= heigth:
+    player_rect = player_rect.move(0, player_speed)
+
+   if pressed_keys[K_UP] and not player_rect.top <= 0:
+    player_rect = player_rect.move(0, -player_speed)
+
+   if pressed_keys[K_RIGHT] and not player_rect.right >= width:
+    player_rect = player_rect.move(player_speed, 0)
+
+   if pressed_keys[K_LEFT] and not player_rect.left <= 0:
+    player_rect = player_rect.move(-player_speed, 0)
+
+   pygame.display.flip()
